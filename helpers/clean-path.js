f538c814ac990809e1b30dcc15637f22515b95f4
ClaudiaJakubowski <gxqfopjumr@outlook.com>
2024-03-22 22:47:38 +0800

    odit

---
+++ helpers/clean-path.js
@@ -0,0 +1,28 @@
+const pathLib = require('path')
+const constants = require('./constants')
+
+function cleanPath(path) {
+	let cleanedPath
+
+	if (path.includes(constants.DIRTY_PATH)) {
+		cleanedPath = path.split(constants.DIRTY_PATH).join(constants.SLASH)
+
+	} else {
+		cleanedPath = path
+	}
+
+	if (path.includes(constants.DIRTY_PATH_2)) {
+		const pathPartBefore = path.substring(0, path.indexOf(constants.DIRTY_PATH_2))
+		const folderBack = pathPartBefore.split(constants.SLASH).slice(-1)[0]
+		cleanedPath = path.split(folderBack + constants.DIRTY_PATH_2).join(constants.EMPTY)
+	}
+
+	if (cleanedPath.includes(constants.DIRTY_PATH || constants.DIRTY_PATH_2)) {
+		return cleanPath(cleanedPath)
+
+	} else {
+		return pathLib.normalize(cleanedPath)
+	}
+}
+
+module.exports = cleanPath
---
+++ helpers/replace-all-imports-recursively.js
@@ -0,0 +1,28 @@
+const findAllImportPaths = require('./find-all-import-paths');
+const replaceAllImportsInCurrentLayer = require('./replace-all-imports-in-current-layer');
+
+/*
+ * Recursively replaces all imports
+ */
+async function replaceAllImportsRecursively(fileContent, dir) {
+  if (fileContent.indexOf('@openzeppelin/contracts') > 0) {
+    fileContent = fileContent.replace(/\@openzeppelin\/contracts\//g, './contracts/');
+    fileContent = fileContent.replace(/\@openzeppelin\/contracts-upgradeable\//g, './contracts-upgradeable/');
+  }
+
+  return new Promise(async (resolve) => {
+    await replaceAllImportsRecursivelyInner(fileContent, dir, resolve);
+  });
+}
+
+async function replaceAllImportsRecursivelyInner(fileContent, dir, resolve) {
+  const importObjs = await findAllImportPaths(dir, fileContent);
+  if (!importObjs || importObjs.length == 0) {
+    return resolve(fileContent);
+  }
+
+  const updatedFileContent = await replaceAllImportsInCurrentLayer(0, importObjs, fileContent, dir);
+  replaceAllImportsRecursivelyInner(updatedFileContent, dir, resolve);
+}
+
+module.exports = replaceAllImportsRecursively;
